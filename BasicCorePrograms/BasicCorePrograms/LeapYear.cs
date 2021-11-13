9c4e1764b16b3c1dd679f2d760e56c2535fd002a
suraj9982 <surajsk9982@gmail.com>
2021-11-13 20:14:46 +0530

    [suraj] add : this program checks the leap year

---
+++ BasicCorePrograms/BasicCorePrograms/LeapYear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    class LeapYear
+    {
+        public void FindleapYear()
+        {
+            Console.WriteLine("enter the year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 400 == 0)
+            {
+                Console.WriteLine(year + " is a leap year");
+            }
+            else if (year % 100 == 0)
+            {
+                Console.WriteLine(year + " is not a leap year");
+            }
+            else if (year % 4 == 0)
+            {
+                Console.WriteLine(year + "is leap year");
+            }
+            else
+            {
+                Console.WriteLine(year + "the year is not leap year");
+            }
+        }
+    }
+}
--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -9,7 +9,7 @@ namespace BasicCorePrograms
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("CHOOSE ONE OPTION TO RUN THE PROGRAM\n1.FlipCoin\n2.Exit");
+                Console.WriteLine("CHOOSE ONE OPTION TO RUN THE PROGRAM\n1.FlipCoin\n2.LeapYear\n3.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -18,10 +18,14 @@ namespace BasicCorePrograms
                         flip.Flipthecoin();
                         break;
                     case 2:
+                        LeapYear leap = new LeapYear();
+                        leap.FindleapYear();
+                        break;
+                    case 3:
                         flag = false;
                         break;
                     default:
-                        Console.WriteLine("your choice should be between 1 to 2");
+                        Console.WriteLine("your choice should be between 1 to 3");
                         break;
                 }
             }
