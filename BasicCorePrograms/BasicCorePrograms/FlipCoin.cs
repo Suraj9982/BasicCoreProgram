b6f6f87d285c0b2c7f07e34174ccf9c66247f7c7
suraj9982 <surajsk9982@gmail.com>
2021-11-13 20:04:28 +0530

    [suraj] add : this program simulate the flip coin game and gives percentage

---
+++ BasicCorePrograms/BasicCorePrograms/FlipCoin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class FlipCoin
+    {
+        /// <summary>
+        /// This flipthecoin method is a simulation of flip coin game.
+        /// This method flip the coin till the user wants to flip. 
+        /// This method also give the percentage of heads and tails
+        /// </summary>
+        public void Flipthecoin()
+        {
+            Console.WriteLine("enter the number of times to flip the coin");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            double headcount = 0, tailcount = 0;
+            for (int i = 0; i < num; i++)
+            {
+                double check = random.NextDouble();
+                if (check < 0.5)
+                {
+                    Console.WriteLine("heads");
+                    headcount++;
+                }
+                else
+                {
+                    Console.WriteLine("tails");
+                    tailcount++;
+                }
+            }
+            double headpercentage = (headcount / num) * 100;
+            Console.WriteLine("Head percenatage is in % =" + headpercentage);
+            double tailpercentage = (tailcount / num) * 100;
+            Console.WriteLine("Tail percenatage is in % =" + tailpercentage);
+        }
+    }
+}
---
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("CHOOSE ONE OPTION TO RUN THE PROGRAM\n1.FlipCoin\n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin flip = new FlipCoin();
+                        flip.Flipthecoin();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("your choice should be between 1 to 2");
+                        break;
+                }
+            }
+        }
+    }
+}
